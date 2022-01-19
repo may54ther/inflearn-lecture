76b70efe681c289b36dbf7947b898fecb5907874
ahakim <aha9654@gmail.com>
2022-01-19 18:54:37 +0900

    @RequestBody 문자 반환

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,6 +19,15 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+    @GetMapping("hello-string")
+    @ResponseBody
+    // ResponseBody : HTTP Body에 문자 내용을 직접 반환 → viewResolver 사용X
+    public String helloString(@RequestParam(value = "name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello "+name;
+    }
+
+
 }
 
 
