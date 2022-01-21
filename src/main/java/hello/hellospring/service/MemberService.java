42798a411f32ac333b466872bcf9f4210e5dfea7
ahakim <aha9654@gmail.com>
2022-01-21 20:33:48 +0900

    검증코드 메서드로 분리

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -12,12 +12,16 @@ public class MemberService {
     // 회원가입
     public Long join(Member member){
         // 회원 이름은 유일, 동명이인 불가
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
-
-        memberRepository.save(member);
-        return member.getId();
     }
 }
