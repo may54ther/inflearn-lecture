c75dc73f1e73fa064c4d50b6945c2313772dba7c
ahakim <aha9654@gmail.com>
2022-01-24 15:11:54 +0900

    스프링 빈 등록 -> 자바 코드로 직접 스프링 빈 등록

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository {
     // MEMO : 실무에서는 동시성 문제가 있어서 주로 화살표 다음에 표기된 타입을 사용
     private static Map<Long, Member> store = new HashMap<>(); // HashMap → ConcurrentHashMap
