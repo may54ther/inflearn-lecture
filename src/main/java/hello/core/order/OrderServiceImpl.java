50a9a4e470426f744000e0977c3c7441cf7d2c82
ahakim <aha9654@gmail.com>
2022-02-07 19:39:38 +0900

    관심사 분리: dip, ocp 위반 해결

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,9 +9,21 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    /* MEMO [SOLID 정책 위반; Ln14~15]
+    *  1. DIP 위반: OrderServiceImpl는  인터페이스(DiscountPolicy), 인터페이스의 구체화 클래스(RateDiscountPolicty) 모두 의존
+    *  2. OCP 위반: 할인 정책을 변경하기 위해 OrderServiceImp 코드가 변경
+    *  위반사항의 해결을 위해 아래처럼 코드를 재작성.
+    * */
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
