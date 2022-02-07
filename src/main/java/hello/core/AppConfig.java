073832004d656423f285e1afe58a43b92a1befac
ahakim <aha9654@gmail.com>
2022-02-07 19:49:28 +0900

    역할, 구현 구분을 위해 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,12 +11,18 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    // AppConfig 리팩터링: 역할, 구현의 구분이 확실해짐.
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
-        // 생성자 주입 : 생성자를 통해 객체를 주입
+        return new MemberServiceImpl(memberRepository());
+    }
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), DiscountPolicy());
+    }
+    private DiscountPolicy DiscountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
