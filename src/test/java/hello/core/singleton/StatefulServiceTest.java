854e7ffe13a3aa1e5285ef4d8d99114daf39eb9b
ahakim <aha9654@gmail.com>
2022-02-10 19:32:16 +0900

    상태를 유지할 경우 발생하는 문제 해결방법

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -15,15 +15,16 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         //ThreadA: 사용자 A -> 10,000원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice= statefulService1.order("userA", 10000);
         //ThreadA: 사용자 B -> 20,000원 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice= statefulService2.order("userB", 20000);
 
         //ThreadA: 사용자 A의 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("userA.price = " + price);
+//        int price = statefulService1;
+        System.out.println("userA.price = " + userAPrice);
+        System.out.println("userB.price = " + userBPrice);
 
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
 
     static class TestConfig {
