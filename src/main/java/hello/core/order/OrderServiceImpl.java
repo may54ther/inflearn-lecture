43bb064a30d4aae1b8aa19dee041a361073644a5
ahakim <aha9654@gmail.com>
2022-02-14 20:27:17 +0900

    lombok의 @RequiredArgsConstructor이 생성자 자동 생성

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,28 +6,23 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
-    // private final MemberRepository memberRepository = new MemoryMemberRepository();
-    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    /* MEMO [SOLID 정책 위반; Ln14~15]
-    *  1. DIP 위반: OrderServiceImpl는  인터페이스(DiscountPolicy), 인터페이스의 구체화 클래스(RateDiscountPolicty) 모두 의존
-    *  2. OCP 위반: 할인 정책을 변경하기 위해 OrderServiceImp 코드가 변경
-    *  위반사항의 해결을 위해 아래처럼 코드를 재작성.
-    * */
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//   Lombok -> @RequiredArgsConstructor : 아래 생성자 코드를 자동 생성해줌.
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
