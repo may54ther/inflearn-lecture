19d4e6416cbf4c85ad86bb3dc90e13f983ef5fe6
ahakim <aha9654@gmail.com>
2022-02-17 18:34:17 +0900

    빈 생명주기 콜백 지원방법 3: 애노테이션

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
 
@@ -25,6 +28,7 @@ public class NetworkClient {
         System.out.println("close " + url);
     }
 
+    @PostConstruct
     public void init() {
         // 의존 관계 주입이 끝나는 시점
         System.out.println("NetworkClient.init");
@@ -32,6 +36,7 @@ public class NetworkClient {
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
