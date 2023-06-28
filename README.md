f97e8df4d88431df990324d817f4f8a71976f473
may54ther <aha9654@gmail.com>
2023-06-28 16:38:56 +0900

    add: spring project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring-DB1
+"김영한의 [스프링 DB 1편 - 데이터 접근 핵심 원리](https://www.inflearn.com/course/스프링-db-1)" 강의 소스코드
---
+++ src/test/java/io/ahakim/db1/Db1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ahakim.db1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Db1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
